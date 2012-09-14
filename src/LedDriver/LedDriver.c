eab147adffa47ef7146402dc2d7ce07867bc1640
davisford <davisford@gmail.com>
2012-09-14 09:32:03 -0400

    working...

--- src/LedDriver/LedDriver.c
+++ src/LedDriver/LedDriver.c
@@ -1,3 +1,4 @@
+#include "LedDriver.h"
 #include <stdint.h>
 
 void LedDriver_Create(uint16_t * address)
---
+++ tests/RunAllTests.c
@@ -0,0 +1,6 @@
+#include "CppUTest/CommandLineTestRunner.h"
+
+int main(int argc, char** argv)
+{
+    return RUN_ALL_TESTS(argc, argv);
+}
